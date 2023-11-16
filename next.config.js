8456b632b5e05062c028793e2203c92d219ec5b2
Veronica Manchola <veronica.manchola@hotmail.com>
2023-11-15 22:16:28 -0300

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+let envImageUnoptimize = process.env.NODE_ENV !== 'production' ? false : true;
+
 const nextConfig = {
   generateBuildId: async () => {
     return 'prod';
+  },
+  output: process.env.NODE_ENV !== 'production' ? undefined : 'export',
+  images: {
+    unoptimized: envImageUnoptimize,
   },
 };
 
